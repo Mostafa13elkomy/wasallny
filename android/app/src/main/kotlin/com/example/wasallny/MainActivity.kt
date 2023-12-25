f272b91897af341721616788b0c954b3cbc18e55
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-12-25 11:01:22 +0200

    create app router and add main colors

---
+++ android/app/src/main/kotlin/com/example/wasallny/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wasallny
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app_router.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:wasallny/constants/my_strings.dart';
+import 'presentation/login_screen/login_screen_view.dart';
+
+class AppRouter {
+    Route?  generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case loginScreen:
+        return MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        );
+    }
+  }
+}
