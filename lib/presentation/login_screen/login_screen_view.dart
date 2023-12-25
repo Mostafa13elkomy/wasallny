f6ac32d5aea50931864b1473c51fc47050c5cfde
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-12-25 11:22:09 +0200

    build intro text in login screen

--- lib/presentation/login_screen/login_screen_view.dart
+++ lib/presentation/login_screen/login_screen_view.dart
@@ -3,8 +3,42 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
+  Widget _buildIntroTexts() {
+    return  Column(
+      children: [
+        const Text(
+          'What is your phone number',
+          style: TextStyle(color: Colors.black,
+          fontSize: 24,
+          fontWeight: FontWeight.bold),
+        ),
+      const  SizedBox(height: 30,),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 2),
+          child:const Text('Please enter your phone number to verify your account',
+          style:  TextStyle(color: Colors.black,
+          fontSize: 18),) ,
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: Colors.white,
+      body: Form(
+        key: UniqueKey(),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 32, vertical: 88),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+            _buildIntroTexts(),
+          ]),
+        ),
+      ),
+    ));
   }
 }
