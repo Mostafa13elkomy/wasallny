6aa059a2dbdc85efa971d4a48b55da5399a697f8
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-12-29 00:55:15 +0200

    create otp screen and add intro text

--- lib/presentation/otp_screen/otp_screen_view.dart
+++ lib/presentation/otp_screen/otp_screen_view.dart
@@ -5,6 +5,17 @@ class OtpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          margin: EdgeInsets.symmetric(horizontal: 32,vertical: 88),
+          child: Column(
+            children: [
+              
+            ],
+          ),
+        ),
+      ));
   }
 }
---
+++ lib/presentation/otp_screen/widgets/intro_text.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:wasallny/constants/my_constants.dart';
+
+class OtpIntroText extends StatelessWidget {
+  OtpIntroText({super.key});
+late final phoneNumber;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'What is your phone number',
+          style: TextStyle(
+              color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 2),
+          child: RichText(
+            text: TextSpan(
+              text: 'Enter your 6 digit code numbers sent to',
+              style:const TextStyle(color: Colors.black, fontSize: 18, height: 1.4),
+              children: <TextSpan>[
+                TextSpan(
+                  text: '$phoneNumber',
+                  style: const TextStyle(
+                    color:MyColors.blue,
+                    fontSize: 18
+                  )
+                )
+              ]
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
