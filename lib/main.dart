99569cfe98809715808bfb82128a3e461257a1a6
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-12-28 22:39:22 +0200

    create phone form field

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:wasallny/firebase_options.dart';
 import 'app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+
+
   runApp(MyApp(
     appRouter: AppRouter(),
   ));
@@ -21,6 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      color: Colors.white,
       theme: ThemeData(primarySwatch: Colors.blue),
       onGenerateRoute: appRouter.generateRoute,
     );
--- lib/presentation/login_screen/login_screen_view.dart
+++ lib/presentation/login_screen/login_screen_view.dart
@@ -4,23 +4,84 @@ class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   Widget _buildIntroTexts() {
-    return  Column(
+    return Column(
       children: [
         const Text(
           'What is your phone number',
-          style: TextStyle(color: Colors.black,
-          fontSize: 24,
-          fontWeight: FontWeight.bold),
+          style: TextStyle(
+              color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
         ),
-      const  SizedBox(height: 30,),
+        const SizedBox(
+          height: 30,
+        ),
         Container(
           margin: const EdgeInsets.symmetric(horizontal: 2),
-          child:const Text('Please enter your phone number to verify your account',
-          style:  TextStyle(color: Colors.black,
-          fontSize: 18),) ,
+          child: const Text(
+            'Please enter your phone number to verify your account',
+            style: TextStyle(color: Colors.black, fontSize: 18),
+          ),
         )
       ],
     );
+  }
+
+  Widget _buildPhoneFormField() {
+    return Row(
+      children: [
+        Expanded(
+            flex: 1,
+            child: Container(
+              height: 60,
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.blue),
+                borderRadius: const BorderRadius.all(Radius.circular(6)),
+              ),
+              child: Text(
+                generateCountryFlag() + ' +20',
+                style: const TextStyle(fontSize: 18, letterSpacing: 2.0),
+              ),
+            )),
+        const SizedBox(
+          width: 16,
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            height: 60,
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 2),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.blue),
+              borderRadius: const BorderRadius.all(Radius.circular(6)),
+            ),
+            child: TextFormField(
+              maxLength: 11,
+              maxLines: 1,
+              autofocus: true,
+              style: TextStyle(fontSize: 18, letterSpacing: 2.0),
+              decoration: InputDecoration(border: InputBorder.none),
+              cursorColor: Colors.black,
+              keyboardType: TextInputType.phone,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Please enter your phone number';
+                } else if (value.length < 11) {
+                  return ' This number is too short';
+                }
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  String generateCountryFlag() {
+    String countryName = 'eg';
+    String flag = countryName.toUpperCase().replaceAllMapped(RegExp(r'[A-Z]'),
+        (match) => String.fromCharCode(match.group(0)!.codeUnitAt(0) + 127397));
+    return flag;
   }
 
   @override
@@ -31,11 +92,14 @@ class LoginScreen extends StatelessWidget {
       body: Form(
         key: UniqueKey(),
         child: Container(
+          color: Colors.white,
           margin: const EdgeInsets.symmetric(horizontal: 32, vertical: 88),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
             _buildIntroTexts(),
+            SizedBox(
+              height: 55,
+            ),
+            _buildPhoneFormField(),
           ]),
         ),
       ),
